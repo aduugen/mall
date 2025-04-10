284b03afefe1c2bef014a549103a50d08a60e137
aduugen <z_semail@163.com>
2025-04-10 15:39:24 +0800

    上传图片无法显示问题

--- mall-portal/src/main/java/com/macro/mall/portal/config/WebMvcConfig.java
+++ mall-portal/src/main/java/com/macro/mall/portal/config/WebMvcConfig.java
@@ -2,18 +2,33 @@ package com.macro.mall.portal.config;
 
 import com.macro.mall.portal.component.VisitorInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebMvcConfig implements WebMvcConfigurer { 
+public class WebMvcConfig implements WebMvcConfigurer {
        @Autowired
        private VisitorInterceptor visitorInterceptor;
-      
+
+       @Value("${upload.path}")
+       private String uploadPath;
+
        @Override
-       public void addInterceptors(InterceptorRegistry registry) {        
-            registry.addInterceptor(visitorInterceptor)
-                    .addPathPatterns("/home/content");
+       public void addInterceptors(InterceptorRegistry registry) {
+              registry.addInterceptor(visitorInterceptor)
+                            .addPathPatterns("/home/content");
+       }
+
+       @Override
+       public void addResourceHandlers(ResourceHandlerRegistry registry) {
+              // 添加外部文件夹映射，将/pics/**映射到物理路径
+              registry.addResourceHandler("/pics/**")
+                            .addResourceLocations("file:" + uploadPath + "/");
+
+              // 打印日志，便于调试
+              System.out.println("添加静态资源映射: /pics/** -> " + "file:" + uploadPath + "/");
        }
 }
